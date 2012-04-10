5364839ab5c26730cd094f5ef7ffc132a03cdf52
Michael Foster <michael@hashlance.com>
2012-04-10 12:13:35 +1000

    allow ctcp version response to be overridden by yakrc

--- plg/ctcp_version/ctcp_version.c
+++ plg/ctcp_version/ctcp_version.c
@@ -1,11 +1,20 @@
 #include "ircproto.h"
 #include "yak.h"
+#include "pref.h"
+#include "stddef.h"
 #include "plg/ctcp/ctcp.h"
 
 static void version(char *usr, char *src, char *msg,
 		char **caps, int ncaps)
 {
-	ircproto_notice(src, "\001VERSION yak %s\001", YAK_VERSION);
+	char *version;
+	
+	version = pref_get("version");
+	
+	if(version == NULL)
+		ircproto_notice(src, "\001VERSION yak %s\001", YAK_VERSION);
+	else
+		ircproto_notice(src, "\001VERSION %s\001", version);
 }
 
 int init()
